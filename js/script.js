fb5703d95aa02f0b7ff1e6429f5c738679f9c3c6
marcoche88 <marco.che@hotmail.it>
2021-07-22 15:25:08 +0200

    boilerplate + css + js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*
+Descrizione:
+Proviamo a ripetere quanto visto in classe, creando la struttura che riteniamo più
+adeguata per rappresentare una carta di Magic.
+Una volta definita la struttura, stampiamo sulla pagina HTML tutte le informazioni
+relative alla carta stessa, senza particolare attenzione a
+dettagli grafici (va bene una lista coi tag UL e LI)
+BONUS: provare a creare una funzione che stampi la carta in pagina.
+*/
