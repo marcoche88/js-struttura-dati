725a8a352f56dd627c471a2e0a376a735c12a2a4
marcoche88 <marco.che@hotmail.it>
2021-07-22 17:18:09 +0200

    function getCard

--- js/script.js
+++ js/script.js
@@ -58,90 +58,98 @@ console.log(card);
 // variabile html
 const cardDisplay = document.getElementById("cards");
 
-// verifica se è presente il sottotipo della carta
-const cardSubType = card.subType ? `- ${card.subType}` : ``;
-
-// verificare quante abilità sono presenti nella carta
-let cardAbilities = `-`;
-if (card.abilities) {
-    cardAbilities = `<ul>`;
-    for (let i = 0; i < card.abilities.length; i++) {
-        cardAbilities += `<li> ${card.abilities[i].launchCost.join(" - ")}: ${card.abilities[i].description} </li>`;
-    }
-    cardAbilities += `</ul>`;
-}
-
-// verifica se è presente il flavortext della carta
-const cardFlavorText = card.flavorText ? `<li><strong>testo di colore:</strong> ${card.flavorText.quote} - ${card.flavorText.authorName}</li>` : ``;
-
-// verifica colore bordo carta
-let cardBorder;
-if (card.cardBorderColor === "#000000") {
-    cardBorder = "Bianco";
-} else {
-    cardBorder = "Nero";
-}
-
-// verifica colore sfondo
-let cardBgColor;
-switch (card.background.color.toLowerCase()) {
-    case "red":
-        cardBgColor = "Rosso";
-        break;
-    case "white":
-        cardBgColor = "Bianco";
-        break;
-    case "blue":
-        cardBgColor = "Blu";
-        break;
-    case "green":
-        cardBgColor = "Verde";
-        break;
-    case "black":
-        cardBgColor = "Nero";
-        break;
-    default:
-        cardBgColor = "-";
-}
-
-// stampare in pagina la descrizione della carta
-const cardTemplate = `
-<ul class="card">
-    <li><strong>id carta:</strong> ${card.id}</li>
-    <li><strong>nome carta:</strong> ${card.name}</li>
-    <li><strong>costo di lancio:</strong> ${card.launchCost.join(" - ")}</li>
-    <li><strong>costo di mana convertito (cmc):</strong> ${card.convertedManaCost}</li>
-    <li>
-        <strong>illustrazione:</strong>
-        <ul>
-            <li>Immagine: ${card.illustration.source}</li>
-            <li>Autore: ${card.illustration.author.name} (id: ${card.illustration.author.id})</li>
-        </ul> 
-    </li>
-    <li><strong>Tipo di carta:</strong> ${card.cardType} ${cardSubType}</li>
-    <li>
-        <strong>espansione:</strong>
-        <ul>
-            <li>Nome: ${card.expansion.name} (id: ${card.expansion.id})</li>
-            <li>Rarità: ${card.expansion.rarity}</li>
-            <li>Numero: ${card.expansion.cardNumber}/${card.expansion.totalCard}</li>
-        </ul>
-    </li>
-    <li>
-        <strong>abilità:</strong>
-        ${cardAbilities}
-    </li>
-    ${cardFlavorText}
-    <li><strong>forza e costituzione:</strong> ${card.strength}/${card.constitution}</li>
-    <li><strong>colore bordo carta:</strong> ${cardBorder}</li>
-    <li>
-        <strong>sfondo carta:</strong>
-        <ul>
-            <li>Colore di sfondo: ${cardBgColor}</li>
-            <li>Immagine di sfondo: ${card.background.source}</li>
-        </ul>
-    </li>
-</ul>
-`;
+const cardTemplate = getCard(card);
 
 cardDisplay.innerHTML = cardTemplate;
+
+// FUNZIONI
+
+function getCard(obj) {
+    // verifica se è presente il sottotipo della carta
+    const cardSubType = obj.subType ? `- ${obj.subType}` : ``;
+
+    // verificare quante abilità sono presenti nella carta
+    let cardAbilities = `-`;
+    if (obj.abilities) {
+        cardAbilities = `<ul>`;
+        for (let i = 0; i < obj.abilities.length; i++) {
+            cardAbilities += `<li> ${obj.abilities[i].launchCost.join(" - ")}: ${obj.abilities[i].description} </li>`;
+        }
+        cardAbilities += `</ul>`;
+    }
+
+    // verifica se è presente il flavortext della carta
+    const cardFlavorText = obj.flavorText ? `<li><strong>testo di colore:</strong> ${obj.flavorText.quote} - ${obj.flavorText.authorName}</li>` : ``;
+
+    // verifica colore bordo carta
+    let cardBorder;
+    if (obj.cardBorderColor === "#000000") {
+        cardBorder = "Nero";
+    } else {
+        cardBorder = "Bianco";
+    }
+
+    // verifica colore sfondo
+    let cardBgColor;
+    switch (obj.background.color.toLowerCase()) {
+        case "red":
+            cardBgColor = "Rosso";
+            break;
+        case "white":
+            cardBgColor = "Bianco";
+            break;
+        case "blue":
+            cardBgColor = "Blu";
+            break;
+        case "green":
+            cardBgColor = "Verde";
+            break;
+        case "black":
+            cardBgColor = "Nero";
+            break;
+        default:
+            cardBgColor = "-";
+    }
+
+    // costruzione template da stamapre
+    const template = `
+    <ul class="card">
+        <li><strong>id carta:</strong> ${obj.id}</li>
+        <li><strong>nome carta:</strong> ${obj.name}</li>
+        <li><strong>costo di lancio:</strong> ${obj.launchCost.join(" - ")}</li>
+        <li><strong>costo di mana convertito (cmc):</strong> ${obj.convertedManaCost}</li>
+        <li>
+            <strong>illustrazione:</strong>
+            <ul>
+                <li>Immagine: ${obj.illustration.source}</li>
+                <li>Autore: ${obj.illustration.author.name} (id: ${obj.illustration.author.id})</li>
+            </ul> 
+        </li>
+        <li><strong>Tipo di carta:</strong> ${obj.cardType} ${cardSubType}</li>
+        <li>
+            <strong>espansione:</strong>
+            <ul>
+                <li>Nome: ${obj.expansion.name} (id: ${obj.expansion.id})</li>
+                <li>Rarità: ${obj.expansion.rarity}</li>
+                <li>Numero: ${obj.expansion.cardNumber}/${obj.expansion.totalCard}</li>
+            </ul>
+        </li>
+        <li>
+            <strong>abilità:</strong>
+            ${cardAbilities}
+        </li>
+        ${cardFlavorText}
+        <li><strong>forza e costituzione:</strong> ${obj.strength}/${obj.constitution}</li>
+        <li><strong>colore bordo carta:</strong> ${cardBorder}</li>
+        <li>
+            <strong>sfondo carta:</strong>
+            <ul>
+                <li>Colore di sfondo: ${cardBgColor}</li>
+                <li>Immagine di sfondo: ${obj.background.source}</li>
+            </ul>
+        </li>
+    </ul>
+    `;
+
+    return template;
+};
